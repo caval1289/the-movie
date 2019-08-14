16edf024c9cf1141dbb4b2f781cd8f709ed512d5
Alessandro Cavaleri <caval1289@gmail.com>
2019-08-14 17:59:16 +0200

    instalation

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = function() {
+    return(
+    <div>
+       Salut les amis
+    </div>
+    )
+};
+
+ReactDOM.render(<App/>,document.querySelector('.container'));
