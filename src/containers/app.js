42e3365c8ca0792bd08dc19780813eb3f81ff7f5
Alessandro Cavaleri <caval1289@gmail.com>
2019-08-14 22:35:37 +0200

    state

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SearchBar from '../components/search-bar'
 
-const App = function() {
+const App = () => {
     return(
     <div>
-       Salut les amis
+       <SearchBar/>
     </div>
     )
 };
