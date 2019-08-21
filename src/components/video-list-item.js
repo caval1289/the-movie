d7b31071499c75a58c1e61f3f78b9c5b47fad3f9
Alessandro Cavaleri <caval1289@gmail.com>
2019-08-22 00:12:22 +0200

    parent enfant

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
 
-const VideoListItem = ({ movie }) => {
+const VideoListItem = (props) => {
+    const { movie } = props;
+
     return (
-        <li className="list-group-item">
+        <li className="list-group-item" onClick={handleOnCLick}>
             <div className="media">
                 <div className="media-left">
                     <img className="media-object img-rounded" height="100px" width="100px" src={`${IMAGE_BASE_URL}${movie.poster_path}`} />
@@ -15,7 +17,12 @@ const VideoListItem = ({ movie }) => {
                 </div>
             </div>
         </li>
+
     )
+    function handleOnCLick() {
+        props.callback(movie);
+
+    }
 }
 
 export default VideoListItem;
